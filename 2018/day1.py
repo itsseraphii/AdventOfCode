8133d91f045a8f69b3f827bb2131f29784d1666d
Jordan G. <itsseraphii@gmail.com>
2025-12-09 00:33:57 -0500

    Added all previously completed years

---
+++ 2018/day1.py
@@ -0,0 +1,32 @@
+from utils.aoc_utils import AOCDay
+
+class Day1(AOCDay):
+    def common(self):
+        self.frequency = 0
+        return 0
+
+    def part1(self):
+        for val in self.inputData:
+            self.changeFrequency(val)
+        return self.frequency
+
+    def part2(self):
+        self.frequency = 0
+        frequencySet = set()
+        loopNum = 0
+        while(True):
+            index = loopNum % len(self.inputData)
+            self.changeFrequency(self.inputData[index])
+            loopNum += 1
+
+            if self.frequency in frequencySet:
+                return self.frequency
+            else:
+                frequencySet.add(self.frequency)
+            pass
+
+    def changeFrequency(self, input):
+        if input[0] == '+':
+            self.frequency += int(input[1:])
+        else:
+            self.frequency -= int(input[1:])
---
+++ 2022/day10.py
@@ -0,0 +1,44 @@
+from utils.aoc_utils import AOCDay
+class Day10(AOCDay):
+    def common(self):
+        self.cycle = 1
+        self.register = 1
+        self.instructions = list(map(lambda x: (x[0:4], int(x[5:]) if x[3] != 'p' else 0), self.inputData))
+        self.signalStrength = []
+        self.CRT = []
+
+        for instruction, value in self.instructions:
+            if instruction == 'noop':
+                self.computeCycle()
+            elif instruction == 'addx':
+                self.computeCycle()
+                self.computeCycle()
+                self.register += value
+
+        return 0
+
+    def part1(self):
+        return sum(map(lambda x: x[0]*x[1], self.signalStrength))
+
+    def part2(self):
+        # Split CRT into lines
+        screen = []
+        for i in range(0, len(self.CRT), 40):
+            screen.append(self.CRT[i:i+40])
+        
+        for line in screen:
+            print(''.join(line))
+
+        return 0
+
+    def computeCycle(self):
+        sprite = (self.register - 1, self.register, self.register + 1)
+        if self.cycle % 40 in sprite:
+            self.CRT.append('#')
+        else:
+            self.CRT.append('.')
+
+        if (((self.cycle - 20) % 40 ) == 0):
+            self.signalStrength.append((self.cycle, self.register))
+
+        self.cycle += 1
